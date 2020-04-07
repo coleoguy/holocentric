578c6df0668119e83ffae5bd2b9f87bcc6020f00
Heath Blackmon <coleoguy@gmail.com>
2020-04-07 11:01:24 -0500

    here you go

---
+++ learning.tocode.funstuff.R
@@ -0,0 +1,62 @@
+# Heath pretending to be Michelle or Sarah
+
+library(ape)
+# read in the data and the tree
+trees <- read.nexus("misof.backbone.nex")
+dat <- read.csv("data.invert.csv", as.is=T)[,-c(6:9)]
+
+
+# TODO compare tree and data and get just the overlap
+
+
+
+library(chromePlus)
+
+# slim the data to include only the desired data
+chrom <- data.frame(dat$binom, dat$haoloid.num, dat$chromosome, stringsAsFactors = F)
+colnames(chrom) <- c("species", "haploid", "chrom")
+chrom$chrom[chrom$chrom == "mono"] <- 0
+chrom$chrom[chrom$chrom == "holo"] <- 1
+chrom <- chrom[chrom$haploid != "#VALUE!", ]
+chrom$haploid <- as.numeric(chrom$haploid)
+chrom$chrom <- as.numeric(chrom$chrom)
+### range should be larger than that observed in the
+### data however it should be as small as possible without
+### impacting inference to speed computation
+# check the range of chromosome numbers present
+range(chrom$haploid)
+
+chroms <- datatoMatrix(chrom, range=c(1,6), hyper=T)
+
+# set the MCMC chain length
+iter <- 5
+library(diversitree)
+
+### w is best set by doing a short run and getting some idea of the
+### confidence interval on these parameters
+lk.mk <- make.mkn(trees[[1]], states=chroms, k=ncol(chroms), strict=F, control=list(method="ode"))
+
+### This sets up the model described in the paper
+con.lk.mk<-constrainMkn(data=chroms, lik=lk.mk, hyper=T,
+                        polyploidy=F, verbose=F,
+                        constrain=list(drop.demi=T, drop.poly=T))
+temp <- mcmc(con.lk.mk, x.init=runif(6,0,10), w=1, nsteps=iter/10)
+w <- diff(sapply(temp[1:6], quantile, c(.05, .95)))
+
+
+### this will cycle through all 100 trees
+results <- list()
+for(i in 1:100){
+  # make the initial likelihood function
+  lk.mk <- make.mkn(tree[[i]], states = chroms, k = ncol(chroms),
+                    strict = F, control = list(method = "ode"))
+  # constrain to a biologically realistic model of chrom evolution
+  con.lk.mk<-constrainMkn(data = chroms, lik = lk.mk, hyper = T,
+                          polyploidy = F, verbose = F,
+                          constrain = list(drop.demi = T, drop.poly = T))
+  # run the MCMC
+  results[[i]] <- mcmc(con.lk.mk, x.init = colMeans(temp)[1:6],
+                       w = w, nsteps = iter)
+}
+
+
