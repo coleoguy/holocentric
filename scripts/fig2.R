9286ed3dacce0310087ef44a31bad05f0cb1a90e
Heath Blackmon <coleoguy@gmail.com>
2020-04-21 10:12:41 -0500

    misc.

---
+++ scripts/fig2.R
@@ -0,0 +1,29 @@
+library(ggraptR)
+library(ggplot2)
+#ggraptR(results.wop)
+
+
+load(file="../results/munged.data.RData")
+#Plot of results without polyploidy
+ggplot(results.wop, aes(y=rate, x=as.factor(type))) +
+  geom_boxplot(aes(fill=as.factor(order)), stat="boxplot",
+               alpha=0.5, width=0.2, position=position_dodge(0.5)) +
+  theme_bw() +
+  theme(text=element_text(family="sans", face="plain", color="#000000",
+                          size=15, hjust=0.5, vjust=0.5),
+        panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  guides(fill=guide_legend(title="Order")) +
+   xlab("Type of Rearrangement") + ylab("Rate")
+
+#Plot of results with polyploidy
+ggplot(results.wp, aes(y=rate, x=as.factor(type))) +
+  geom_boxplot(aes(fill=as.factor(order)), stat="boxplot",
+               alpha=0.5, width=0.2, position=position_dodge(0.5)) +
+  theme_bw() +
+  theme(text=element_text(family="sans", face="plain", color="#000000",
+                          size=15, hjust=0.5, vjust=0.5),
+        panel.border = element_blank(), panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +
+  guides(fill=guide_legend(title="Order")) +
+  xlab("Type of Rearrangement") + ylab("Rate")
