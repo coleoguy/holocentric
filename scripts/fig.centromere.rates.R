5f922978138e3d518b829ec21562a1da0ea57ec1
Heath Blackmon <coleoguy@gmail.com>
2020-04-24 13:57:34 -0500

    fig update

--- scripts/fig.centromere.rates.R
+++ scripts/fig.centromere.rates.R
@@ -5,7 +5,11 @@ plot(0,0,col="white",
      xlim=c(-.02,.02),
      xlab="rate difference",
      ylab="density")
-abline(v=0)
+abline(v=0, lty=3,col="red")
 polygon(density(rates$asc1-rates$asc2,bw=.001), col=rgb(1,0,0,.1))
 polygon(density(rates$desc1-rates$desc2,bw=.001),col=rgb(0,1,0,.1))
 polygon(density(rates$pol1-rates$pol2,bw=.001),col=rgb(0,0,1,.1))
+points(pch=22, bg=c(rgb(1,0,0,.2), rgb(0,1,0,.2), rgb(0,0,1,.2)),
+       x=rep(.01,3), y=c(400,370,340))
+text(x=rep(.01,3), y=c(400,370,340), pos=4, cex=.7,labels=c("ascending","descending","polyploidy"))
+# export this as pdf at 4x4
