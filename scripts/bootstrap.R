e8da402082eb310c4d51e20d4dc37c81db5e8923
Michelle Jonika <Michelle19@tamu.edu>
2020-08-15 17:00:04 -0500

    Update script

--- scripts/bootstrap.R
+++ scripts/bootstrap.R
@@ -1,4 +1,7 @@
-rates <- f.results
+#read in data
+rates <- load("../results/cent.rates-backbone1.RData")
+
+
 starts <- seq(from=1, by=25, length.out = 100)
 ends <- seq(from=25, by=25, length.out = 100)
 treerows <- matrix(, 25, 100)
@@ -6,6 +9,9 @@ for(i in 1:100){
   treerows[,i] <- starts[i]:ends[i]
 }
 sig.count <- 0
+
+library(coda)
+dists <- list()
 for(i in 1:1000){
   sel.trees <- sample(1:100, replace=T)
   sel.rows <- as.vector(treerows[,sel.trees])
@@ -13,8 +19,31 @@ for(i in 1:1000){
      HPDinterval(as.mcmc(rates$asc2[sel.rows]))[1]){
     sig.count <- sig.count + 1
   }
+ dists[[i]] <- matrix(c(rates$asc1[sel.rows], rates$asc2[sel.rows]),
+                      2500, 2)
 }
 
+plot(0,0,col="white",
+     ylim=c(-5,110),
+     xlim=c(-.05,.1),
+     xlab=expression(paste(Delta, R[x])),
+     ylab="density")
+abline(v=0, lty=3,col="black")
+cols <- rainbow(1000)
+for(i in 1:1000){
+rtemp <- dists[[i]][,1] + dists[[i]][,2]
+
+lines(density(rtemp, bw = .0005), col = cols[1])}
+polygon(density(fusion, bw = .0005),col = cols[2])
+polygon(density(poly, bw = .0005),col = cols[3])
+points(pch = 22, bg = cols,
+       x = rep(-.02, 3), y = c(700, 650, 600))
+text(x = rep(-.02, 3), y = c(700, 650, 600), pos = 4, cex = .7,
+     labels=c("fission", "fusion", "polyploidy"))
+
+lines(y=rep(-15, 2), x=hpdfis, col=cols[1], lwd=4)
+lines(y=rep(-37, 2), x=hpdfus, col=cols[2], lwd=4)
+lines(y=rep(-59, 2), x=hpdpol, col=cols[3], lwd=4)
 
 
 
