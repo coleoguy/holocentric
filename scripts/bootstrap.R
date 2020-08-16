6946b2a48ab8e5dfceae903689ba4cbd76fde62a
Heath Blackmon <coleoguy@gmail.com>
2020-08-16 16:45:21 -0500

    many edits

--- scripts/bootstrap.R
+++ scripts/bootstrap.R
@@ -1,60 +1,86 @@
 #read in data
 rates <- load("../results/cent.rates-backbone1.RData")
 
-
 starts <- seq(from=1, by=25, length.out = 100)
 ends <- seq(from=25, by=25, length.out = 100)
 treerows <- matrix(, 25, 100)
 for(i in 1:100){
   treerows[,i] <- starts[i]:ends[i]
 }
-sig.count <- 0
 
+
+# MAKE SUP PLOTS
+
+# get fissions
 library(coda)
-dists <- list()
+dists <- matrix(NA,2500,1000)
 for(i in 1:1000){
   sel.trees <- sample(1:100, replace=T)
   sel.rows <- as.vector(treerows[,sel.trees])
-  if(HPDinterval(as.mcmc(rates$asc1[sel.rows]))[2] <
-     HPDinterval(as.mcmc(rates$asc2[sel.rows]))[1]){
-    sig.count <- sig.count + 1
-  }
- dists[[i]] <- matrix(c(rates$asc1[sel.rows], rates$asc2[sel.rows]),
-                      2500, 2)
+  dists[, i] <- rates$asc1[sel.rows] - rates$asc2[sel.rows]
+}
+par(mfcol=c(1,3))
+# plot fissions
+library(viridis)
+plot(0,0,col="white",
+     ylim=c(-5,105),
+     xlim=c(-.025,.04),
+     xlab=expression(paste(Delta, R[fission])),
+     ylab="density")
+cols <- sample(viridis(1000))
+lows <- highs <- c()
+for(i in 1:1000){
+  lines(density(dists[, i], bw = .0015), col = cols[i],lwd=.09)
+  highs[i] <- HPDinterval(as.mcmc(dists[,i]))[2]
+  lows[i] <- HPDinterval(as.mcmc(dists[,i]))[1]
+}
+lines(density(rates$asc1 - rates$asc2,, bw = .0015), lty=3, lwd=2)
+lines(y=c(-5,-5),x=c(max(lows),min(highs)),lwd=3)
+
+# get fusions
+dists <- matrix(NA,2500,1000)
+for(i in 1:1000){
+  sel.trees <- sample(1:100, replace=T)
+  sel.rows <- as.vector(treerows[,sel.trees])
+  dists[, i] <- rates$desc1[sel.rows] - rates$desc2[sel.rows]
 }
 
+# plot fusions
 plot(0,0,col="white",
-     ylim=c(-5,110),
-     xlim=c(-.05,.1),
-     xlab=expression(paste(Delta, R[x])),
+     ylim=c(-5,165),
+     xlim=c(-.025,.03),
+     xlab=expression(paste(Delta, R[fusion])),
      ylab="density")
-abline(v=0, lty=3,col="black")
-cols <- rainbow(1000)
 for(i in 1:1000){
-rtemp <- dists[[i]][,1] + dists[[i]][,2]
+  lines(density(dists[, i], bw = .0015), col = cols[i],lwd=.09)
+  highs[i] <- HPDinterval(as.mcmc(dists[,i]))[2]
+  lows[i] <- HPDinterval(as.mcmc(dists[,i]))[1]
+}
+lines(density(rates$desc1 - rates$desc2,, bw = .0015), lty=3, lwd=2)
+lines(y=c(-5,-5),x=c(max(lows),min(highs)),lwd=3)
 
-lines(density(rtemp, bw = .0005), col = cols[1])}
-polygon(density(fusion, bw = .0005),col = cols[2])
-polygon(density(poly, bw = .0005),col = cols[3])
-points(pch = 22, bg = cols,
-       x = rep(-.02, 3), y = c(700, 650, 600))
-text(x = rep(-.02, 3), y = c(700, 650, 600), pos = 4, cex = .7,
-     labels=c("fission", "fusion", "polyploidy"))
+# get polyploidy
+dists <- matrix(NA,2500,1000)
+for(i in 1:1000){
+  sel.trees <- sample(1:100, replace=T)
+  sel.rows <- as.vector(treerows[,sel.trees])
+  dists[, i] <- rates$pol1[sel.rows] - rates$pol2[sel.rows]
+}
 
-lines(y=rep(-15, 2), x=hpdfis, col=cols[1], lwd=4)
-lines(y=rep(-37, 2), x=hpdfus, col=cols[2], lwd=4)
-lines(y=rep(-59, 2), x=hpdpol, col=cols[3], lwd=4)
+# plot polyploidy
+plot(0,0,col="white",
+     ylim=c(-5,1400),
+     xlim=c(-.002,.002),
+     xlab=expression(paste(Delta, R[polyploidy])),
+     ylab="density")
+for(i in 1:1000){
+  lines(density(dists[, i], bw = .0001), col = cols[i],lwd=.09)
+  highs[i] <- HPDinterval(as.mcmc(dists[,i]))[2]
+  lows[i] <- HPDinterval(as.mcmc(dists[,i]))[1]
+}
+lines(density(rates$pol1 - rates$pol2,, bw = .0001), lty=3, lwd=2)
+lines(y=c(-5,-5),x=c(max(lows),min(highs)),lwd=3)
+
+# export 10" wide by 4" tall
 
 
-
-
-
-
-abline(v=starts, col="red", lwd=.3)
-lines(rates$asc2, col="blue")
-library(coda)
-HPDinterval(as.mcmc(rates$asc1[1:25]))
-HPDinterval(as.mcmc(rates$asc2[1:25]))
-
-plot(rates$asc1~rates$asc2, pch=16, cex=.3)
-summary(lm(rates$asc1~rates$asc2))
